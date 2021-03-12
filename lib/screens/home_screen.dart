002607e66c963191d1896cf879f71f9e4f4a9beb
Aakash <akashjagani4899@gmail.com>
2021-03-12 16:15:38 +0530

    initial commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
-
-class Item {
-  Item({
-    this.expandedValue,
-    this.headerValue,
-    this.isExpanded = false,
-  });
-
-  String expandedValue;
-  String headerValue;
-  bool isExpanded;
-}
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+class MyItem {
+  MyItem({this.isExpanded: false, this.header, this.body});
+
+  bool isExpanded;
+  final String header;
+  final String body;
+}
+
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   bool _flag = true;
   Animation<double> _myAnimation;
   AnimationController _controller;
+
+  List<MyItem> _items = <MyItem>[
+    MyItem(header: 'header 1', body: 'body 1'),
+    MyItem(header: 'header 2', body: 'body 2'),
+    MyItem(header: 'header 3', body: 'body 3'),
+    MyItem(header: 'header 4', body: 'body 4'),
+    MyItem(header: 'header 5', body: 'body 5'),
+    MyItem(header: 'header 6', body: 'body 6'),
+    MyItem(header: 'header 7', body: 'body 7'),
+  ];
 
   @override
   void initState() {
@@ -39,6 +46,52 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 5,
+          left: 3,
+          right: 3,
+        ),
+        child: ListView(
+          children: [
+            ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  _items[index].isExpanded = !_items[index].isExpanded;
+                });
+              },
+              children: _items.map((MyItem item) {
+                return ExpansionPanel(
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                      ),
+                      child: Container(
+                        height: 70,
+                        width: double.infinity,
+                        child: Row(
+                          children: [
+                            Icon(Icons.home),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(item.header),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  isExpanded: item.isExpanded,
+                  body: Container(
+                    child: Text(item.body),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
