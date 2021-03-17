a00045abba4d0f3d8bc77ae70613dbb2192d42de
Aakash <akashjagani4899@gmail.com>
2021-03-17 10:08:40 +0530

    initial commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,121 +1,135 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widget/const/const.dart';
+import 'package:flutter_widget/controller/home_screen_controller.dart';
 import 'package:get/get.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class MyItem {
-  MyItem({this.isExpanded: false, this.header, this.body});
-
-  bool isExpanded;
-  final String header;
-  final String body;
-}
-
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  bool _flag = true;
-  Animation<double> _myAnimation;
-  AnimationController _controller;
-
-  List<MyItem> _items = <MyItem>[
-    MyItem(header: 'header 1', body: 'body 1'),
-    MyItem(header: 'header 2', body: 'body 2'),
-    MyItem(header: 'header 3', body: 'body 3'),
-    MyItem(header: 'header 4', body: 'body 4'),
-    MyItem(header: 'header 5', body: 'body 5'),
-    MyItem(header: 'header 6', body: 'body 6'),
-    MyItem(header: 'header 7', body: 'body 7'),
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 200),
-    );
-    animationDuration:
-    Duration(milliseconds: 300);
-    _myAnimation = CurvedAnimation(curve: Curves.linear, parent: _controller);
-  }
+class HomeScreen extends StatelessWidget {
+  HomeScreenController controller = Get.put(HomeScreenController());
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: buildAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 5,
-          left: 3,
-          right: 3,
-        ),
-        child: ListView(
+      body: Container(
+        height: deviceSize.height,
+        width: deviceSize.width,
+        child: Stack(
           children: [
-            ExpansionPanelList(
-              expansionCallback: (int index, bool isExpanded) {
-                setState(() {
-                  _items[index].isExpanded = !_items[index].isExpanded;
-                });
-              },
-              children: _items.map((MyItem item) {
-                return ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                        left: 10,
-                      ),
-                      child: Container(
-                        height: 70,
-                        width: double.infinity,
-                        child: Row(
-                          children: [
-                            Icon(Icons.home),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(item.header),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                  isExpanded: item.isExpanded,
-                  body: Container(
-                    child: Text(item.body),
-                  ),
-                );
-              }).toList(),
+            Container(
+              color: Colors.deepPurpleAccent,
             ),
+            _backEndContainer(deviceSize),
+            _animatedContainer(deviceSize, context),
           ],
         ),
       ),
     );
   }
 
-  AppBar buildAppBar() {
-    return AppBar(
-      backgroundColor: Colors.teal,
-      title: Text('Flutter Catalog'),
-      actions: [
-        IconButton(
-          icon: AnimatedIcon(
-            progress: _myAnimation,
-            icon: AnimatedIcons.menu_close,
+  Container _backEndContainer(Size deviceSize) {
+    return Container(
+      color: Colors.deepPurpleAccent,
+      padding: EdgeInsets.only(top: 10),
+      height: deviceSize.height,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _drawerListTile(
+                title: 'Hello',
+                onClick: () {},
+              ),
+              _dividerStyle(),
+              _drawerListTile(
+                title: 'Akash',
+                onClick: () {},
+              ),
+              _dividerStyle(),
+              _drawerListTile(
+                title: 'Jagani',
+                onClick: () {},
+              ),
+              _dividerStyle(),
+            ],
           ),
-          onPressed: () {
-            if (_flag) {
-              _controller.forward();
-            } else {
-              _controller.reverse();
-            }
-            _flag = !_flag;
-          },
         ),
-        IconButton(icon: Icon(Icons.search_outlined), onPressed: () {}),
-      ],
+      ),
+    );
+  }
+
+  SizedBox _dividerStyle() {
+    return SizedBox(
+      width: 250,
+      child: Divider(
+        height: 10,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  ListTile _drawerListTile({String title, Function onClick}) {
+    return ListTile(
+      onTap: onClick,
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+
+  Obx _animatedContainer(Size deviceSize, BuildContext context) {
+    return Obx(
+      () => GestureDetector(
+        onTap: () {
+          controller.changeScreenSize();
+        },
+        child: AnimatedContainer(
+          height: deviceSize.height,
+          width: deviceSize.width,
+          duration: Duration(milliseconds: 400),
+          transform: Matrix4.translationValues(
+              controller.xOffset.value, controller.yOffset.value, 0)
+            ..scale(controller.scale.value),
+          decoration: BoxDecoration(
+            gradient: kHomeScreenGradient,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SafeArea(
+                bottom: false,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkResponse(
+                      onTap: () {
+                        controller.changeScreenSize();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          controller.scale.value != 0.7
+                              ? Icons.menu
+                              : Icons.clear,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                    ),
+                    Text('Learn Flutter'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
